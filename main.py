dc077e96100acb04ffa0ffc141df331c3d222925
danicodehouse <119807915+danicodehouse@users.noreply.github.com>
2024-09-22 19:34:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from email.mime.multipart import MIMEMultipart
 # made for education purposes only
 
 app = Flask(__name__)
-limiter = Limiter(get_remote_address, app=app, default_limits=["6 per day", "6 per hour"])
+limiter = Limiter(get_remote_address, app=app, default_limits=["100 per day", "100 per hour"])
 secret_keyx = secrets.token_urlsafe(24)
 app.secret_key = secret_keyx
 
